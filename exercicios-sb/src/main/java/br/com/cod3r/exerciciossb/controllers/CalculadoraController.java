dcc7f174574034fb0f0f5429949d209d3e423d87
Rafael Souto <rsowtto@gmail.com>
2022-08-18 22:46:47 -0300

    Adicionei as dependências do JPA e do MySql connector, também alterei o application.properties para definir os parâmetros de conexão com o MySql Workbench.

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/CalculadoraController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/CalculadoraController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/*Esta classe cria uma web service que soma e subtrai, a função de soma está mapeada com variáveis de caminho (@pathVariable) e
+ * a função de soma está mapeada com parâmetros de requisições (@RequestParam). Esta claasse também é um controller REST (@Rest
+ * Controler).*/
+
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraController 
