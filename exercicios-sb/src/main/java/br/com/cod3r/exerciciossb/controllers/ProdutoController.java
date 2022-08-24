450d684702a397a0d3ba0e1e780e6c95096e862a
Rafael Souto <rsowtto@gmail.com>
2022-08-23 22:30:02 -0300

    Criei os métodos de consulta/leitura, tanto por id quanto por selecionar todos os registros da tabela de produtos no banco de dados. Também usei a biblioteca javax.validation.constraints para aplicar algumas validações em cima dos registros de novos produtos.

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,9 +1,13 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +24,36 @@ import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 	 * seja enviada.
 	 * O ProdutoRepository save(S entity) irá salvar o objeto produto no banco de dados de forma devidamente
 	 * mapeada, e o JSON retornado (Objetos retornados para o browser são convertidos em JSON) terá id
-	 * equivalente ao id do novo registro gerado no banco de dados springbhoot dentro do MySql Workbnech.*/
-	@PostMapping @ResponseBody public Produto novoProduto(@RequestParam String nome)
+	 * equivalente ao id do novo registro gerado no banco de dados springboot dentro do MySql Workbnech.
+	 * Veja que agora, eu sisplesmente retirei os vários parâmetros do método novoProduto(@RequestParam Strin nome, @RequestParam double preco, 
+	 * @RequestParam double desconto), agora sua assinatura é novoProduto(Produto produto), e note também que dentro do método, eu não preciso mais criar
+	 * um objeto Produto passando os parâmetros, eu sisplesmente declarei que irei receber um produto e salvar este produto na linha 29. O Spring Boot é
+	 * suficientemente inteligente para pegar os parâmetros da requisição post e inseri-los no objeto de forma automática.
+	 * A anotação @Valid serve para aplicar as validações (dentro de javax.validation.constraints) declaradas na classe 
+	 * Produto em cima de seus atributos. Caso a requisição post não esteja de acordo com as validações declaradas. Ocorrerá
+	 * uma BadRequest, que acontece no lado cliente. E caso o @Valid não seja declarado em cima do parâmetro Produto produto, ocorrerá um erro 500 e
+	 * a aplicação irá quebrar e ficar fora do ar*/
+	@PostMapping @ResponseBody public Produto novoProduto(@Valid Produto produto)
 	{
-		Produto produto = new Produto(nome);
 		produtoRepository.save(produto);
 		return produto;
+	}
+	
+	/*Uma reqiosição get para a url api/produtos irá chamar este método.*/
+	@GetMapping
+	public Iterable<Produto> obterProdutos()
+	{
+		return produtoRepository.findAll();
+	}
+	
+	/*http://localhost:8080/api/produtos/número inteiro. Esse método é ativado por uma requisição get, porém com um / seguido
+	 * do parâmetro inteiro para achar o objeto pesquisado pelo id. Caso o id passado de parâmetro não esteja associado à 
+	 * nenhum registro da tabela dentro do banco de dados, o JSON retornado será vazio, mas a aplicação não irá quebrar, pois
+	 * o objeto Optional<T> serve justamente para este tipo de situação, para evitar NullPointerException. Lembrando que
+	 * ao especificar o path do método http get, é preciso usar a anotação @PathVariable sobre o parâmetro passado.*/
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id)
+	{
+		return produtoRepository.findById(id);
 	}
 }
--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
@@ -4,17 +4,44 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 @Entity public class Produto 
 {
 	@Id() @GeneratedValue(strategy = GenerationType.IDENTITY) private int id;
 	
+	/* Anotação @NotBlank do pacote javax.validation.constraints, serve para não permitir valor vazio, não nulo,
+	é como o nullable = false do JPA, mas é uma anotação fornecida pelo próprio Java. Porém, a biblioteca javax.validation.constraints 
+	precisa ser adiquirida pela dependêcia <groupId>javax.validation</groupId><artifactId>validation-api</artifactId> no 
+	pom.xml*/
+	@NotBlank
 	private String nome;
+	
+	/* Anotação @Min (value = 0) do pacote javax.validation.constraints, serve para declarar valor mínimo, e não nulo
+	 * obviamente, é uma anotação fornecida pelo próprio Java. Porém, a biblioteca javax.validation.constraints precisa
+	 * ser adiquirida pela dependêcia <groupId>javax.validation</groupId><artifactId>validation-api</artifactId> no pom.xml*/
+	@Min(value = 0)
+	private double preco;
+	
+	/* Anotação @Max (value = 1) do pacote javax.validation.constraints, serve para declarar valor máximo, e não nulo
+	 * obviamente, é uma anotação fornecida pelo próprio Java. Porém, a biblioteca javax.validation.constraints precisa
+	 * ser adiquirida pela dependêcia <groupId>javax.validation</groupId><artifactId>validation-api</artifactId> no pom.xml*/
+	@Min(value = 0) 
+	@Max(value = 1)
+	private double desconto;
 	
 	public Produto() {}
 	
 	public Produto(String nome) {
 		this.nome = nome;
+	}
+
+	public Produto(String nome, double preco, double desconto)
+	{
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
 	}
 
 	public int getId() {
@@ -31,6 +58,22 @@ import javax.persistence.Id;
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
 	}
 	
 }
